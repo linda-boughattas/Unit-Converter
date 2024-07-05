4fd808c4ed025b94060ec7f61a9b57c1a21c8032
Linda Boughattas <linda.boughattas@enicar.ucar.tn>
2024-07-05 21:05:21 +0200

    Completed JavaScript functionality for converting

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,21 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+const inpitEl=document.getElementById("input")
+const convertBtn=document.getElementById("convert")
+let meterFoot=document.getElementById("meterFoot")
+let literGallon=document.getElementById("literGallon")
+let kiloPound=document.getElementById("kiloPound")
+
+convertBtn.addEventListener("click", function(){
+    const inputValue = parseFloat(inpitEl.value)
+
+    const metersToFeet = Number.parseFloat(inputValue * 3.281).toFixed(2);
+    const feetToMeters = Number.parseFloat(inputValue * 0.305).toFixed(2);
+    const litersToGallons = Number.parseFloat(inputValue * 0.264).toFixed(2);
+    const gallonsToLiters = Number.parseFloat(inputValue * 3.785).toFixed(2);
+    const kilosToPounds = Number.parseFloat(inputValue * 3.281).toFixed(2);
+    const poundsToKilos = Number.parseFloat(inputValue * 0.454).toFixed(2);
+
+    meterFoot.textContent=`${inputValue} meters = ${metersToFeet} feet | ${inputValue} feet = ${feetToMeters} meters`
+    literGallon.textContent=`${inputValue} liters = ${litersToGallons} gallons | ${inputValue} gallons = ${gallonsToLiters} liters`
+    kiloPound.textContent=`${inputValue} kilograms = ${kilosToPounds} pounds | ${inputValue} pounds = ${poundsToKilos} kilos`
+
+})
